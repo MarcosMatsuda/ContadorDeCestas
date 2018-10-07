323c4a70aadc05642b4c88c1750eee47bcae14c7
Marcos Matsuda <marcos.matsuda@MacBook-Pro-de-Marcos.local>
2018-10-07 20:54:38 -0300

    created CicloDeVidaApp - Contador de Cestas

---
+++ CicloDeVidaApp-Kotlin/app/src/main/java/br/com/marcos/matsuda/ciclodevida/MainViewModel.kt
@@ -0,0 +1,11 @@
+package br.com.marcos.matsuda.ciclodevida
+
+import android.arch.lifecycle.ViewModel
+
+
+class MainViewModel: ViewModel() {
+
+    var placarCasa = 0
+    var placarVisitante = 0
+
+}
